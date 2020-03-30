574b742cd5bda8e375f9fbcf5773f629a5409ae6
lqdreality <lqdreality@gmail.com>
2020-03-30 17:30:07 -0400

    WIP

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -2,13 +2,22 @@ import numpy as np
 from scipy.stats import multivariate_normal
 import pandas as pd
 
+def lse(x,axis=None) :
+	if axis is None :
+		x_max = np.max(x)
+		return x_max + np.log(np.sum(np.exp(x-x_max)))
+	else :
+		x_max = np.max(x,axis=axis)
+		return x_max + np.log(np.sum(np.exp(x-x_max[:,np.newaxis]),axis=axis))
+
 def forwards_backwards(X,
 	                   A,
 	                   pi,
 	                   emission,
 	                   emission_params={},
 	                   likelihood=False,
-	                   return_normed=True) :
+	                   return_normed=True,
+	                   log_sum_exp=False) :
 	# Assume inputs are np.arrays for now
 	K = A.shape[0]
 	assert K == len(pi)
@@ -22,7 +31,7 @@ def forwards_backwards(X,
 	for n in range(N) :
 		for k in range(K) :
 			#emissions[n,k] = emission_params[X[n,:],k]
-			emissions[n,k] = emission(X[n,:],k,params=emission_params) 
+			emissions[n,k] = emission(X[n,:],k,params=emission_params)
 	
 	alphas = np.zeros((N,K))
 	betas  = np.zeros((N,K))
@@ -30,11 +39,22 @@ def forwards_backwards(X,
 
 	#\alpha(z_1) = p(x_1,z_1) = p(x_1|z_1)p(z_1)
 	for k in range(K) :
-		alphas[0,k] = pi[k]*emissions[0,k]
+		if log_sum_exp :
+			alphas[0,k] = pi[k] + emissions[0,k]
+		else :
+			alphas[0,k] = pi[k]*emissions[0,k]
 	for na,nb in zip(range(1,N),reversed(range(N-1))) :
 		for k in range(K) :
-			alphas[na,k] = emissions[na,k]*np.sum(alphas[na-1,:]*A[:,k])
-			betas[nb,k] = np.sum(emissions[nb+1,:]*betas[nb+1,:]*A[k,:])
+			if log_sum_exp :
+				alphas[na,k] = emissions[na,k]+lse(alphas[na-1,:]+A[:,k])
+				betas[nb,k] = lse(emissions[nb+1,:]+betas[nb+1,:]+A[k,:])
+			else :
+				alphas[na,k] = emissions[na,k]*np.sum(alphas[na-1,:]*A[:,k])
+				betas[nb,k] = np.sum(emissions[nb+1,:]*betas[nb+1,:]*A[k,:])
+
+	#print(emissions)
+	#print(np.sum(alphas == 0,axis=1))
+	#print(np.sum(betas == 0,axis=1))
 
 	#\gamma(\bm{z}_i) = \frac{1}{p(\bm{X})}\alpha(\bm{z}_i)\beta(\bm{z}_i)
 	#p(\bm{X}) = \sum_{\bm{z}_j\in Supp(\bm{z})}\alpha(\bm{z}_i)\beta(\bm{z}_i)
@@ -42,10 +62,15 @@ def forwards_backwards(X,
 	#normalizing the product \alpha(\bm{z}_i)\beta(\bm{z}_i)). So I find it easier to think of the following
 	#p(\bm{X}) = Z(\bm{X},i)
 	#\gamma(\bm{z}_i) = \frac{1}{Z(\bm{X},i)}\alpha(\bm{z}_i)\beta(\bm{z}_i)
-	gamma_no_norm = alphas*betas
+	if log_sum_exp :
+		gamma_no_norm = alphas + betas
+	else :
+		gamma_no_norm = alphas*betas
 	if return_normed :
-		gammas = gamma_no_norm/np.sum(gamma_no_norm,axis=1)[:,np.newaxis]
-
+		if log_sum_exp :
+			gammas = gamma_no_norm-lse(gamma_no_norm,axis=1)[:,np.newaxis]
+		else :
+			gammas = gamma_no_norm/np.sum(gamma_no_norm,axis=1)[:,np.newaxis]
 	#\xi(\bm{z}_{i-1},\bm{z}_i) = \frac{1}{p(\bm{X})}\alpha(\bm{z}_{i-1})
 	#\beta(\bm{z}_i)p(\bm{x}_i|\bm{z}_i)p(\bm{z}_i|\bm{z}_{i-1})
 	xi_no_norm = np.zeros((N-1,K,K))
@@ -53,9 +78,15 @@ def forwards_backwards(X,
 		xis = xi_no_norm.copy()
 	for n in range(1,N) :
 		for k in range(K) :
-			xi_no_norm[n-1,k,:] = alphas[n-1,k]*betas[n,:]*emissions[n,:]*A[k,:]
+			if log_sum_exp :
+				xi_no_norm[n-1,k,:] = alphas[n-1,k]+betas[n,:]+emissions[n,:]+A[k,:]
+			else :
+				xi_no_norm[n-1,k,:] = alphas[n-1,k]*betas[n,:]*emissions[n,:]*A[k,:]
 		if return_normed :
-			xis[n-1,:,:] =  xi_no_norm[n-1,:,:]/np.sum(xi_no_norm[n-1,:,:])
+			if log_sum_exp :
+				xis[n-1,:,:] =  xi_no_norm[n-1,:,:]-lse(xi_no_norm[n-1,:,:])
+			else :
+				xis[n-1,:,:] =  xi_no_norm[n-1,:,:]/np.sum(xi_no_norm[n-1,:,:])
 			#denominator = np.dot(np.dot(alpha[t, :].T, a) * b[:, V[t + 1]].T, beta[t + 1, :])
 	#print(xis[5,:,:])
 	#print(np.sum(xis,axis=(0,1)))
@@ -87,7 +118,15 @@ class HMM :
 		self.emissions = emissions
 		self.update_initial_state = update_initial_state
 
-	def fit(self,X,stopping_criterion={'num_iters':100}) :
+	def lse(self,x,axis=None) :
+		if axis is None :
+			x_max = np.max(x)
+			return x_max + np.log(np.sum(np.exp(x-x_max)))
+		else :
+			x_max = np.max(x,axis=axis)
+			return x_max + np.log(np.sum(np.exp(x-x_max),axis=axis))
+
+	def fit(self,X,log_sum_exp=False,stopping_criterion={'num_iters':100}) :
 		if 'num_iters' in stopping_criterion :
 			for n in range(stopping_criterion['num_iters']) :
 				gammas,xis,likelihood = forwards_backwards(X,
@@ -96,11 +135,18 @@ class HMM :
 					                                       self.emit,
 					                                       emission_params={},
 					                                       likelihood=True,
-					                                       return_normed=True)
+					                                       return_normed=True,
+					                                       log_sum_exp=log_sum_exp)
 				if self.update_initial_state :
-					self.pi = gammas[0,:]/np.sum(gammas[0,:])
-				self.A = np.sum(xis,axis=0)/(np.sum(xis,axis=(0,2))[:,np.newaxis])
-				self.update_emissions(**{'X':X,'gammas':gammas,'xis':xis})
+					if log_sum_exp :
+						self.pi = gammas[0,:]-self.lse(gammas[0,:])
+					else :
+						self.pi = gammas[0,:]/np.sum(gammas[0,:])
+				if log_sum_exp :
+					self.A = self.lse(xis,axis=0)-(self.lse(xis,axis=(0,2))[:,np.newaxis])
+				else :
+					self.A = np.sum(xis,axis=0)/(np.sum(xis,axis=(0,2))[:,np.newaxis])
+				self.update_emissions(**{'X':X,'gammas':gammas,'xis':xis,'log_sum_exp':log_sum_exp})
 				
 
 	def emit(self,x,z,params={}) :
@@ -124,12 +170,18 @@ class CategoricalHMM(HMM) :
 		return self.emissions[x,z]
 
 	def update_emissions(self,**kwargs) :
+		log_sum_exp = kwargs.get('log_sum_exp',False)
+		print(log_sum_exp)
 		X = kwargs['X']
 		gammas = kwargs['gammas']
 		for i in range(self.emissions.shape[0]) :
 			mask = np.squeeze(X) == i
-			self.emissions[i,:] = np.sum(gammas[mask,:],axis=0)/\
-			                      np.sum(gammas,axis=0)
+			if log_sum_exp :
+				self.emissions[i,:] = self.lse(gammas[mask,:],axis=0)-\
+			                          self.lse(gammas,axis=0)
+			else :
+				self.emissions[i,:] = np.sum(gammas[mask,:],axis=0)/\
+			                          np.sum(gammas,axis=0)
 
 class GaussianHMM(HMM) :
 	def __init__(self,
@@ -157,22 +209,63 @@ class GaussianHMM(HMM) :
 	def update_emissions(self,**kwargs) :
 		X = kwargs['X']
 		gammas = kwargs['gammas']
-		self.emissions['means'] = X.T.dot(gammas)/np.sum(gammas,axis=0)
-		for k in range(gamma.shape[1]) :
+		N = X.shape[0]
+		D = X.shape[1]
+		K = gammas.shape[1]
+		#print(gammas)
+		#(K,N)*(N,D)=(K,D)
+		#self.emissions['means'] = gammas.T.dot(X)/np.sum(gammas,axis=0)[:,np.newaxis]
+		for k in range(K) :
+			mu_totals = np.zeros(D,)
+			for n in range(N) :
+				mu_totals += gammas[n,k]*X[n,:]
+			mu_totals /= np.sum(gammas[:,k])
+			self.emissions['means'][k,:] = mu_totals
+		for k in range(K) :
+			cov_totals = np.zeros((D,D))
+			for n in range(N) :
+				x_mu = X[n,:] - self.emissions['means'][k,:]
+				outer_prod = np.outer(x_mu,x_mu)
+				outer_prod *= gammas[n,k]
+				cov_totals += outer_prod
+			cov_totals /= np.sum(gammas[:,k])
+			self.emissions['covs'][k,:,:] = cov_totals
+		"""
+		for k in range(K) :
 			x_mu = X - self.emissions['means'][k,:] #(N,D) - (D,) = (N,D)
-			self.emissions['covs']  = np.einsum('nd,jn->dj',gammas[:,k]*x_mu,x_mu.T)/np.sum(gammas[:,k])
+			outer_prod = (gammas[:,k]*x_mu.T).dot(x_mu)
+			self.emissions['covs'][k,:,:] = outer_prod/np.sum(gammas[:,k])
+			#self.emissions['covs'][k,:,:] = np.einsum('nd,nj->dj',gammas[:,k,np.newaxis]*x_mu,x_mu)/np.sum(gammas[:,k])
+		"""
 
 def main() :
+	#"""
 	A = np.ones((2, 2))
 	A = A / np.sum(A, axis=1)
 	b = np.array(((1, 3, 5), (2, 4, 6)))
 	b = b / np.sum(b, axis=1).reshape((-1, 1))
 	b = b.T
-	X = pd.read_csv('/home/chrism/sw/hmm/test.csv')
+	X = pd.read_csv('/home/chrism/sw/hmm/data/test.csv')
 	X = X['Visible'].values[:,np.newaxis]
+
 	c_hmm = CategoricalHMM(A=A,pi=np.array([0.5,0.5]),emissions=b,update_initial_state=False)
-	c_hmm.fit(X)
+	c_hmm.fit(X,log_sum_exp=True)
 	print(c_hmm.A)
+	print(np.exp(c_hmm.A))
+	"""
+	A = np.ones((3,3))
+	A /= 3
+	b = {'means':np.array([[0,0],[5,5],[-5,5]],dtype=np.float64),
+	     'covs':np.array([[[1,0],[0,1]],[[1,0],[0,1]],[[1,0],[0,1]]],dtype=np.float64)}
+	#X1 = np.random.multivariate_normal(mean=b['means'][0,:],cov=b['covs'][0,:,:],size=300)
+	#X2 = np.random.multivariate_normal(mean=b['means'][1,:],cov=b['covs'][1,:,:],size=300)
+	#X3 = np.random.multivariate_normal(mean=b['means'][2,:],cov=b['covs'][2,:,:],size=300)
+	#X = np.concatenate((X1,X2,X3))
+	X = np.random.multivariate_normal(mean=np.array([0,0]),cov=np.array([[5,0],[0,5]]),size=900)
+	g_hmm = GaussianHMM(A=A,pi=np.array([0.5,0.5,0.5]),emissions=b,update_initial_state=False)
+	g_hmm.fit(X)
+	print(g_hmm.A)
+	#"""
 
 if __name__ == '__main__' :
 	main()
